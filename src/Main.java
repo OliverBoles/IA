86cc99d65319b345f7d037405c548d633bfecddd
17boles_o <17boles_o@bexleygs.co.uk>
2023-06-29 11:43:34 +0100

    29 June

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        SchoolCalendar sc = new SchoolCalendar();
+        sc.getDate();
         String RoomOpen = Event.room();
         String freeTime = Event.timeBooked();
 
--- src/SchoolCalendar.java
+++ src/SchoolCalendar.java
@@ -4,33 +4,46 @@ import java.util.Scanner;
 public class SchoolCalendar {
     private ArrayList<String> termDays;
 
+    public SchoolCalendar(){
+        termDays=FileHandler.readWholeFile("Dates.CSV");
+    }
+
+
     // is in term time (true false function)
     public boolean isTermDate(String DateRequest){
-        if (FileHandler.containsString("DataBase",DateRequest)) {
-            return true;
-        }
+
         return false;
     }
     // give a day and month and returns string from array list
-    public String getDate(String v) {
-        v = getDay() + getMonth();
-        if (FileHandler.containsString("DataBase",v)) {
-            return FileHandler.readForString("DataBase",v);
+    public String getDate() {
+        String a = getDate();
+        String b = getMonth();
+        String v = a + b;
+        for (int i = 0; i < termDays.size(); i++) {
+            if (termDays.get(i).equals(v)) {
+                return termDays.get(i); // Return the index where the searchTerm is found
+            }
         }
-        return null;
+        return null; // Return null if the searchTerm is not found in the ArrayList
     }
+
     // 2 functions one that returns month and one that returns day
     public String getDay() {
-        return null;
+        Scanner roomRequest = new Scanner(System.in);
+        System.out.println("what room do you want to book");
+        String dayRequest = roomRequest.next();
+        return dayRequest;
     }
 
     public String getMonth() {
-        return null;
+        Scanner roomRequest = new Scanner(System.in);
+        System.out.println("what room do you want to book");
+        String monthRequest = roomRequest.next();
+        return monthRequest;
     }
     // constructor reads from csv file each record in this format
     // wednesday 21 june, thursday 22 june... etc.
 
-    termDays=FileHandler.readWholeFile("Dates.CSV")
 
 
 }
